d795ec832f1a10c4e1abd729aefbb805ced15f96
kubotama <16697573+kubotama@users.noreply.github.com>
2026-06-23 23:23:28 +0900

    test: テスト環境をnodeからhappy-domに変更した

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     globals: true, // describe や it を明示的にインポートせずに使えるようにする
-    environment: 'node',
+    environment: 'happy-dom',
   },
 })
